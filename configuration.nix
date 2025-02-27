00d2b34ca026713a010f46ba337e9755774d50e3
Patrick Hofmann <git@hfmnn.com>
2025-02-27 19:09:11 +0100

    Try nvidia latest

--- configuration.nix
+++ configuration.nix
@@ -83,14 +83,17 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+    # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    # Use the latest development drivers
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    
+    # Add this line to support newer GPUs
+    acceptLicense = true;
   };
 
   # Enable the X11 windowing system.
