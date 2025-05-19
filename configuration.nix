4ca7453a961b2d3e427b0261cf801f50d17b7958
Patrick Hofmann <git@hfmnn.com>
2025-05-19 23:31:20 +0200

    nvidia fixed?

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,10 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "de_DE.UTF-8/UTF-8"
+  ];
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
@@ -101,9 +105,9 @@
       version = "570.144";
       sha256_64bit = "sha256-wLjX7PLiC4N2dnS6uP7k0TI9xVWAJ02Ok0Y16JVfO+Y=";
       sha256_aarch64 = "sha256-6kk2NLeKvG88QH7/YIrDXW4sgl324ddlAyTybvb0BP0=";
-      openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
-      settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
-      persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
+      openSha256 = "sha256-PATw6u6JjybD2OodqbKrvKdkkCFQPMNPjrVYnAZhK/E=";
+      settingsSha256 = "sha256-VcCa3P/v3tDRzDgaY+hLrQSwswvNhsm93anmOhUymvM=";
+      persistencedSha256 = "sha256-hx4w4NkJ0kN7dkKDiSOsdJxj9+NZwRsZEuhqJ5Rq3nM=";
     };
 
   };
@@ -229,6 +233,8 @@
     duf
     gparted
     parted
+    tmux
+    k9s
     
     # 3d printing
     pkgsUnstable.prusa-slicer
@@ -336,6 +342,9 @@
 
   # Set the default editor to vim
   environment.variables.EDITOR = "vim";
+  environment.variables = {
+    LC_ALL = "en_US.UTF-8";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
